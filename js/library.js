1ce8aa69a5c8a147d95646c6d5ba9a475cdf149e
Роман Булатов <r.bulatov@jetmail.cc>
2025-02-15 11:17:25 +0500

    added state minor changes in Bottle temporary moved click handler to main excluded and extended Game excluded intersection calc into lib function minor cleanup in render refactored and commented entry point

---
+++ js/library.js
@@ -0,0 +1,6 @@
+export function isPointInRect(point, rect) {
+  return point.x > rect.x &&
+      point.x < rect.x + rect.width &&
+      point.y > rect.y &&
+      point.y < rect.y + rect.height
+}
--- js/render.js
+++ js/render.js
@@ -1,5 +1,5 @@
-export function drawScene(ctx, canvas, bottles) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    bottles.forEach((bottle) => bottle.draw(ctx));
-    requestAnimationFrame(() => drawScene(ctx, canvas, bottles));
+export function drawScene(ctx, bottles) {
+    ctx.reset();
+    bottles.forEach(bottle => bottle.draw(ctx));
+    requestAnimationFrame(() => drawScene(ctx, bottles));
 }
